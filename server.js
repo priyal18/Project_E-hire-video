6a40c46504eb8d4258e8a9010086841b26cfa252
priyal18 <priyal930gupta@gmail.com>
2021-05-16 15:03:32 +0530

    PATH

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const request = require("request");
 const app = express();
+const path = require('path');
 
 const server = http.createServer(app);
 const socket = require("socket.io");
